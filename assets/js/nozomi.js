bb5c4274fe7b2d80439edf5572db783eb4286f53
NOZOMI <nonnonnoninb@gmail.com>
2022-02-22 20:34:57 +0900

    画像たち

---
+++ assets/js/nozomi.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:2000,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+
+})
