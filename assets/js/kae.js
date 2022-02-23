4472dcb1008902e593f6d44296750078ceffc36d
森木香江 <kaemoriki@gmail.com>
2022-02-23 21:30:59 +0900

    コミット

--- assets/js/kae.js
+++ assets/js/kae.js
@@ -1,12 +1,13 @@
-$(function() {
-    $(".fade").letterfx({
-      "fx": "fade"
-    });
-  });
-
 setTimeout(function () {
     $('.start p').fadeIn(1600);
 }, 500);
 setTimeout(function () {
     $('.start').fadeOut(500);
 }, 2500);
+
+$(function() {
+    $(".fade").letterfx({
+      "fx": "fade"
+    });
+  });
+
